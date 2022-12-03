af211138e3ab6add4cfadfc39f7089f0e50391de
Christopher Fuchs <fuchs-christopher@gmx.de>
2022-12-03 22:17:41 +0100

    added divers stoff

--- script.js
+++ script.js
@@ -1,58 +1,58 @@
 let questions = [
     {
         "question": "Wer hat HTML erfunden?",
-        "answer_1": "Robbie Williams",
-        "answer_2": "Lady Gaga",
-        "answer_3": "Tim Berners-Lee",
-        "answer_4": "Justin Bieber",
+        "answer_1": '<div class="answerButtons"><b>A</b></div>Robbie Williams',
+        "answer_2": '<div class="answerButtons"><b>B</b></div>Lady Gaga',
+        "answer_3": '<div id="answer_Button" class="answerButtons"><b>C</b></div>Tim Berners-Lee',
+        "answer_4": '<div class="answerButtons"><b>D</b></div>Justin Bieber',
         "right_answer": 3
     },
     {
         "question": "Was bedeutet das HTML Tag &lt;a&gt;?",
-        "answer_1": "Text Fett",
-        "answer_2": "Container",
-        "answer_3": "Ein Link",
-        "answer_4": "Kursiv",
+        "answer_1": '<div class="answerButtons"><b>A</b></div>Text Fett',
+        "answer_2": '<div class="answerButtons"><b>B</b></div>Container',
+        "answer_3": '<div id="answer_Button" class="answerButtons"><b>C</b></div>Ein Link',
+        "answer_4": '<div class="answerButtons"><b>D</b></div>Kursiv',
         "right_answer": 3
     },
     {
         "question": "Wie bindet man eine Website in eine Website ein?",
-        "answer_1": "&lt;iframe&gt;, &lt;frame&gt;, and &lt;frameset&gt;",
-        "answer_2": "&lt;iframe&gt;",
-        "answer_3": "&lt;frame&gt;",
-        "answer_4": "&lt;frameset&gt",
+        "answer_1": '<div class="answerButtons"><b>A</b></div>&lt;iframe&gt;, &lt;frame&gt;, and &lt;frameset&gt;',
+        "answer_2": '<div id="answer_Button" class="answerButtons"><b>B</b></div>&lt;iframe&gt;',
+        "answer_3": '<div class="answerButtons"><b>C</b></div>&lt;frame&gt;',
+        "answer_4": '<div class="answerButtons"><b>D</b></div>&lt;frameset&gt',
         "right_answer": 2
     },
     {
         "question": "Wie stellt man einen Text am BESTEN fett dar?",
-        "answer_1": "&lt;strong&gt;",
-        "answer_2": "CSS nutzen",
-        "answer_3": "&lt;bold&gt;",
-        "answer_4": "&lt;b&gt;",
+        "answer_1": '<div id="answer_Button" class="answerButtons"><b>A</b></div>&lt;strong&gt;',
+        "answer_2": '<div class="answerButtons"><b>B</b></div>CSS nutzen;',
+        "answer_3": '<div class="answerButtons"><b>C</b></div>&lt;bold&gt;',
+        "answer_4": '<div class="answerButtons"><b>D</b></div>&lt;b&gt;',
         "right_answer": 1
     },
     {
         "question": "Welches Attribut kann man NICHT für Textarea verwenden?",
-        "answer_1": "readonly",
-        "answer_2": "max",
-        "answer_3": "from",
-        "answer_4": "spellcheck",
+        "answer_1": '<div id="answer_Button" class="answerButtons"><b>A</b></div>readonly',
+        "answer_2": '<div class="answerButtons"><b>B</b></div>max',
+        "answer_3": '<div class="answerButtons"><b>C</b></div>from',
+        "answer_4": '<div class="answerButtons"><b>D</b></div>spellcheck',
         "right_answer": 1
     },
     {
         "question": "Wie wählst du alle Elemente vom Typ &lt;a&gt; mit dem attribut title aus?",
-        "answer_1": "a[title]{...}",
-        "answer_2": "a > title {...}",
-        "answer_3": "a.title {...}",
-        "answer_4": "a=title {...}",
+        "answer_1": '<div id="answer_Button" class="answerButtons"><b>A</b></div>a[title]{...}',
+        "answer_2": '<div class="answerButtons"><b>B</b></div>a > title {...}',
+        "answer_3": '<div class="answerButtons"><b>C</b></div>a.title {...}',
+        "answer_4": '<div class="answerButtons"><b>D</b></div>a=title {...}',
         "right_answer": 1
     },
     {
         "question": "Wie definiert man in JavaScript eine Variable?",
-        "answer_1": "let 100 = rate;",
-        "answer_2": "100 = let rate;",
-        "answer_3": "rate = 100;",
-        "answer_4": "let rate = 100;",
+        "answer_1": '<div class="answerButtons"><b>A</b></div>let 100 = rate;',
+        "answer_2": '<div class="answerButtons"><b>B</b></div>100 = let rate;',
+        "answer_3": '<div class="answerButtons"><b>C</b></div>rate = 100;',
+        "answer_4": '<div id="answer_Button" class="answerButtons"><b>D</b></div>let rate = 100;',
         "right_answer": 4
     }
 ];
@@ -68,15 +68,12 @@ function init() {
 
 
 function showQuestion() {
-
     if (currentQuestion >= questions.length) {
         //TODO: Show End Screen
         document.getElementById('endScreen').style = '';
         document.getElementById('questionBody').style = 'display: none';
     } else {
-
         let question = questions[currentQuestion];
-
         document.getElementById('question-number').innerHTML = currentQuestion + 1;
         document.getElementById('questiontext').innerHTML = question['question'];
         document.getElementById('answer_1').innerHTML = question['answer_1'];
@@ -94,7 +91,9 @@ function answer(selection) {
 
     if (selectionQuestionNumber == question['right_answer']) {
         document.getElementById(selection).parentNode.classList.add('bg-success');
+        document.getElementById('answer_Button').classList.add('bgForButton');
     } else {
+        document.getElementById('answer_Button').classList.add('bgForButton');
         document.getElementById(selection).parentNode.classList.add('bg-danger');
         document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
     }
