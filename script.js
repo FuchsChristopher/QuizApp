1f55c9c3af670f5bf2cab9a6a19a45d381271adc
Christopher Fuchs <fuchs-christopher@gmx.de>
2022-12-07 19:54:04 +0100

    added sounds, cleanCoding

--- script.js
+++ script.js
@@ -1,66 +1,7 @@
-let questions = [
-    {
-        "question": "Wer hat HTML erfunden?",
-        "answer_1": 'Robbie Williams',
-        "answer_2": 'Lady Gaga',
-        "answer_3": 'Tim Berners-Lee',
-        "answer_4": 'Justin Bieber',
-        "right_answer": 3
-    },
-    {
-        "question": "Was bedeutet das HTML Tag &lt;a&gt;?",
-        "answer_1": 'Text Fett',
-        "answer_2": 'Container',
-        "answer_3": 'Ein Link',
-        "answer_4": 'Kursiv',
-        "right_answer": 3
-    },
-    {
-        "question": "Wie bindet man eine Website in eine Website ein?",
-        "answer_1": '&lt;iframe&gt;, &lt;frame&gt;, and &lt;frameset&gt;',
-        "answer_2": '&lt;iframe&gt;',
-        "answer_3": '&lt;frame&gt;',
-        "answer_4": '&lt;frameset&gt',
-        "right_answer": 2
-    },
-    {
-        "question": "Wie stellt man einen Text am BESTEN fett dar?",
-        "answer_1": '&lt;strong&gt;',
-        "answer_2": 'CSS nutzen;',
-        "answer_3": '&lt;bold&gt;',
-        "answer_4": '&lt;b&gt;',
-        "right_answer": 1
-    },
-    {
-        "question": "Welches Attribut kann man NICHT für Textarea verwenden?",
-        "answer_1": 'readonly',
-        "answer_2": 'max',
-        "answer_3": 'from',
-        "answer_4": 'spellcheck',
-        "right_answer": 1
-    },
-    {
-        "question": "Wie wählst du alle Elemente vom Typ &lt;a&gt; mit dem attribut title aus?",
-        "answer_1": 'a[title]{...}',
-        "answer_2": 'a > title {...}',
-        "answer_3": 'a.title {...}',
-        "answer_4": 'a=title {...}',
-        "right_answer": 1
-    },
-    {
-        "question": "Wie definiert man in JavaScript eine Variable?",
-        "answer_1": 'let 100 = rate;',
-        "answer_2": '100 = let rate;',
-        "answer_3": 'rate = 100;',
-        "answer_4": 'let rate = 100;',
-        "right_answer": 4
-    }
-];
-
-
 let rightQuestions = 0;
-
 let currentQuestion = 0;
+let AUDIO_SUCCESS = new Audio('audio/success.mp3');
+let AUDIO_WRONG = new Audio('audio/wrong.mp3');
 
 
 function init() {
@@ -72,34 +13,55 @@ function init() {
 
 
 function showQuestion() {
-    if (currentQuestion >= questions.length) {
-        //Show End Screen
-        document.getElementById('endScreen').style = '';
-        document.getElementById('questionBody').style = 'display: none';
-        document.getElementById('amount-of-questions').innerHTML = questions.length;
-        document.getElementById('amount-right-of-questions').innerHTML = rightQuestions;
+    if (gameIsOver()) {
+        showEndscreen();
     } else {
-        let percent = currentQuestion / (questions.length -1);
-        percent = Math.round(percent * 100);
-        document.getElementById('progessbar').innerHTML = `${percent} %`;
-        document.getElementById('progessbar').style.width = `${percent}%`;
-        
-        let question = questions[currentQuestion];
-        document.getElementById('question-number').innerHTML = currentQuestion + 1;
-        document.getElementById('questiontext').innerHTML = question['question'];
-        document.getElementById('answer_1').innerHTML = `<div class="answerButtons"><b>A</b></div>${question['answer_1']}`;
-        document.getElementById('answer_2').innerHTML = `<div class="answerButtons"><b>B</b></div>${question['answer_2']}`;
-        document.getElementById('answer_3').innerHTML = `<div class="answerButtons"><b>C</b></div>${question['answer_3']}`;
-        document.getElementById('answer_4').innerHTML = `<div class="answerButtons"><b>D</b></div>${question['answer_4']}`;
+        renderProgressbar();
+        updateToNextQuestion();
     }
     enablePointer();
 }
 
 
+function gameIsOver() {
+    return currentQuestion >= questions.length;
+}
+
+
 function showStartScreen() {
+    let mainContent = document.getElementById('mainContainer');
+    mainContent.innerHTML = '';
+    mainContent.innerHTML = renderQuizApp();
     //Show Start Screen
     document.getElementById('startScreen').style = '';
     document.getElementById('questionBody').style = 'display: none';
+}
+
+
+function renderProgressbar() {
+    let percent = currentQuestion / (questions.length - 1);
+    percent = Math.round(percent * 100);
+    document.getElementById('progessbar').innerHTML = `${percent} %`;
+    document.getElementById('progessbar').style.width = `${percent}%`;
+}
+
+
+function updateToNextQuestion() {
+    let question = questions[currentQuestion];
+    document.getElementById('question-number').innerHTML = currentQuestion + 1;
+    document.getElementById('questiontext').innerHTML = question['question'];
+    document.getElementById('answer_1').innerHTML = `<div class="answerButtons"><b>A</b></div>${question['answer_1']}`;
+    document.getElementById('answer_2').innerHTML = `<div class="answerButtons"><b>B</b></div>${question['answer_2']}`;
+    document.getElementById('answer_3').innerHTML = `<div class="answerButtons"><b>C</b></div>${question['answer_3']}`;
+    document.getElementById('answer_4').innerHTML = `<div class="answerButtons"><b>D</b></div>${question['answer_4']}`;
+}
+
+
+function showEndscreen() {
+    document.getElementById('endScreen').style = '';
+    document.getElementById('questionBody').style = 'display: none';
+    document.getElementById('amount-of-questions').innerHTML = questions.length;
+    document.getElementById('amount-right-of-questions').innerHTML = rightQuestions;
 }
 
 
@@ -110,7 +72,7 @@ function restartGame() {
     document.getElementById('next-button').innerHTML = 'Nächste Frage';
     rightQuestions = 0;
     currentQuestion = 0;
-    
+
 }
 
 
@@ -119,22 +81,41 @@ function answer(selection) {
     let selectionQuestionNumber = selection.slice(-1);
     let idOfRightAnswer = `answer_${question['right_answer']}`;
     if (selectionQuestionNumber == question['right_answer']) {
-        document.getElementById(selection).firstChild.classList.add('bgForButton');
-        document.getElementById(selection).parentNode.classList.add('bg-success');
-        rightQuestions++;
+        rightAnswer(selection);
         disabledPointer();
     } else {
-        document.getElementById(idOfRightAnswer).firstChild.classList.add('bgForButton');
-        document.getElementById(selection).parentNode.classList.add('bg-danger');
-        document.getElementById(selection).firstChild.classList.add('bgForButton2');
-        document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
+        wrongAnswer(idOfRightAnswer, selection);
         disabledPointer();
     }
-
     if (currentQuestion == 6) {
         document.getElementById('next-button').innerHTML = 'Finsh';
     }
     document.getElementById('next-button').disabled = false;
+}
+
+
+function rightAnswer(selection) {
+    document.getElementById(selection).firstChild.classList.add('bgForButton');
+    document.getElementById(selection).parentNode.classList.add('bg-success');
+    rightQuestions++;
+    AUDIO_SUCCESS.play();
+}
+
+
+function wrongAnswer(idOfRightAnswer, selection) {
+    document.getElementById(idOfRightAnswer).firstChild.classList.add('bgForButton');
+    document.getElementById(selection).parentNode.classList.add('bg-danger');
+    document.getElementById(selection).firstChild.classList.add('bgForButton2');
+    document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
+    AUDIO_WRONG.play();
+}
+
+
+function nextQuestion() {
+    currentQuestion++; // z.B. von 0 auf 1
+    document.getElementById('next-button').disabled = true;
+    resetAnswerButtons();
+    showQuestion();
 }
 
 
@@ -151,14 +132,6 @@ function disabledPointer() {
     document.getElementById('answerDiv2').style.pointerEvents = 'none';
     document.getElementById('answerDiv3').style.pointerEvents = 'none';
     document.getElementById('answerDiv4').style.pointerEvents = 'none';
-}
-
-
-function nextQuestion() {
-    currentQuestion++; // z.B. von 0 auf 1
-    document.getElementById('next-button').disabled = true;
-    resetAnswerButtons();
-    showQuestion();
 }
 
 
---
+++ templates.js
@@ -0,0 +1,89 @@
+function renderQuizApp() {
+    return `
+    <div class="card2 quiz-card">
+
+            <div class="bgc-nav">
+                <div class="navImg">
+                    <img class="imgSize" src="./img/logo.png">
+                </div>
+                <nav class="navLink">
+                    <div class="hover"><a href="#">HTML</a></div>
+                    <div class="hover"><a href="#">CSS</a></div>
+                    <div class="hover"><a href="#">JS</a></div>
+                    <div class="hover"><a href="#">JAVA</a></div>
+                </nav>
+            </div>
+            <div class="card-body card-body-color" id="startScreen" style="display: none;">
+                <div class="startScreenDiv">
+                    <div class="startScreenDiv2"><b>Welcome to<br>
+                        The Awesome HTML Quiz</b>
+                        Ready for for the Challenge?
+                    </div>
+                    <img class="startScreenImg" src="./img/brainbg.jpg" style="width: 32rem;">
+                    <button class="startButton" onclick="init()">START NOW</button>
+                </div>
+
+            </div>
+            <div class="card-body card-body-color" id="endScreen" style="display: none;">
+                <div class="endScreenDiv">
+                    <img class="endScreenImg" src="./img/brain result.png">
+                    <span>COMPLETE<br>
+                        HTML QUIZ
+                    </span>
+                </div>
+                <div class="endScreenDiv2">
+                    <span style="margin-left: 140px;">YOUR SCORE</span><b id="amount-right-of-questions" style="margin-left: 40px;">1</b>/<b id="amount-of-questions" style="margin-right: 55px;">5</b>
+                    <div><img class="endScreenImg2" src="./img/tropy.png"></div>
+                </div>
+                <div class="endScreenDiv3">
+                    <button>SHARE</button>
+                    <button onclick="restartGame()">REPLAY</button>
+                </div>
+                
+            </div>
+
+            <div class="card-body2 card-body-color" id="questionBody">
+                <h5 class="card-title" id="questiontext">Frage</h5>
+
+                <div class="card quiz-answer-card mb-2" id="answerDiv1" onclick="answer('answer_1')">
+                    <div class="card-body" id="answer_1">
+                        Antwort
+                    </div>
+                </div>
+
+                <div class="card quiz-answer-card mb-2" id="answerDiv2" onclick="answer('answer_2')">
+                    <div class="card-body" id="answer_2">
+                        Antwort
+                    </div>
+                </div>
+
+
+                <div class="card quiz-answer-card mb-2" id="answerDiv3" onclick="answer('answer_3')">
+                    <div class="card-body" id="answer_3">
+                        Antwort
+                    </div>
+                </div>
+
+
+                <div class="card quiz-answer-card mb-2" id="answerDiv4" onclick="answer('answer_4')">
+                    <div class="card-body" id="answer_4">
+                        Antwort
+                    </div>
+                </div>
+
+                <div class="question-footer">
+                    <span>
+                        <b id="question-number">1</b> von <b id="all-questions">5</b> Fragen
+                    </span>
+
+                    <button onclick="nextQuestion()" class="btn btn-primary" id="next-button" disabled>Nächste
+                        Frage</button>
+                </div>
+                <div class="progress">
+                    <div class="progress-bar" id="progessbar" role="progressbar" aria-label="Example with label" style="width: 25%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">25%</div>
+                  </div>
+
+            </div>
+        </div>
+    `;
+}
